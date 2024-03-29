ffd57a0ee06b5f79cc11d006816f0f8deb16627f
Peter Kaltzis <peterkaltzis@outlook.com>
2024-03-29 15:44:13 +1100

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import type { BookieData } from "./utils/Scraper.js";
 import { Pointsbet } from "./Scrapers/Pointsbet.js";
 
-// Initialising arbot.
+// Initialising maret data.
 const marketData: MarketData = {
     meta: {
         scrapedAt: Date.now()
@@ -9,10 +9,23 @@ const marketData: MarketData = {
     data: []
 };
 
-const scraper = new Pointsbet();
-const data = await scraper.scrapeBookie();
-console.log(data);
-marketData.data.push(data);
+// Collecting scrapers.
+const scrapers = [
+    new Pointsbet()
+];
+
+// Scraping market data.
+const promises: Promise<void>[] = [];
+for (const scraper of scrapers) {
+    promises.push(scraper.scrapeBookie().then((bookieData) => {
+        marketData.data.push(bookieData);
+    }).catch((e: unknown) => {
+        console.log(e);
+    }));
+}
+
+await Promise.all(promises);
+console.log(marketData);
 
 interface MarketData {
     meta: {
