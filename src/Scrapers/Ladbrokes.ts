0c1dfc134910e30240c26f3b2af0f2424ee7b8d9
Peter Kaltzis <peterkaltzis@outlook.com>
2024-03-31 00:20:53 +1100

    added Ladbrokes scraper headers

--- src/Scrapers/Ladbrokes.ts
+++ src/Scrapers/Ladbrokes.ts
@@ -6,15 +6,15 @@ export class Ladbrokes extends Scraper {
 
     protected bookieEndpoints = {
         RugbyLeague: {
-            NRL: "https://api.ladbrokes.com.au/v2/sport/event-request?category_ids=%5B%22608a1803-45bc-465a-8471-c89dcb68a27d%22%5D&competition_id=3e85a456-59b5-4363-95e6-836854492fdf&include_any_team_vs_any_team_events=true"
+            NRL: "https://api.ladbrokes.com.au/v2/sport/event-request?category_ids=%5B%22608a1803-45bc-465a-8471-c89dcb68a27d%22%5D&competition_id=3e85a456-59b5-4363-95e6-836854492fdf"
         },
         AussieRules: {
-            AFL: "https://api.ladbrokes.com.au/v2/sport/event-request?category_ids=%5B%2223d497e6-8aab-4309-905b-9421f42c9bc5%22%5D&competition_id=ccff2e9a-5347-41aa-902a-bb6b1886d817&include_any_team_vs_any_team_events=true"
+            AFL: "https://api.ladbrokes.com.au/v2/sport/event-request?category_ids=%5B%2223d497e6-8aab-4309-905b-9421f42c9bc5%22%5D&competition_id=ccff2e9a-5347-41aa-902a-bb6b1886d817"
         },
         Basketball: {
-            NBA: "https://api.ladbrokes.com.au/v2/sport/event-request?category_ids=%5B%223c34d075-dc14-436d-bfc4-9272a49c2b39%22%5D&competition_id=2d20a25b-6b96-4651-a523-442834136e2d&include_any_team_vs_any_team_events=true"
+            NBA: "https://api.ladbrokes.com.au/v2/sport/event-request?category_ids=%5B%223c34d075-dc14-436d-bfc4-9272a49c2b39%22%5D"
         }
-    };
+    }
 
     protected async scrapeComp(sportId: string, compId: string, url: string): Promise<CompData> {
         const data = await Scraper.getDataFromUrl(url) as MatchesResponse;
@@ -22,7 +22,7 @@ export class Ladbrokes extends Scraper {
         const promises: Promise<void>[] = [];
         for (const eventId in data.events) {
             const event = data.events[eventId];
-            if (event.event_type.name === "Match" && !event.name.includes("Specials")) {
+            if (event.event_type.name === "Match" && !event.name.includes("Specials") && event.competition.name === compId) {
                 const teams = event.name.split(" vs ");
                 const match = new Match(
                     compId,
@@ -30,16 +30,13 @@ export class Ladbrokes extends Scraper {
                     teams[1],
                     Date.parse(event.actual_start)
                 );
-                // Checking whether match has already occurred.
-                if ((Date.now() - match.startTime) / (1000 * 60 * 60) < 4) {
-                    promises.push(this.scrapeMarkets(compId, `https://api.ladbrokes.com.au/v2/sport/event-card?id=${event.id}`).then((matchOffers) => {
-                        match.offers = matchOffers;
-                        comp[match.id] = match;
-                    }).catch((e: unknown) => {
-                        console.error(e);
-                        comp[match.id] = match;
-                    }));
-                }
+                promises.push(this.scrapeMarkets(compId, `https://api.ladbrokes.com.au/v2/sport/event-card?id=${event.id}`).then((matchOffers) => {
+                    match.offers = matchOffers;
+                    comp[match.id] = match;
+                }).catch((e: unknown) => {
+                    console.error(e);
+                    comp[match.id] = match;
+                }));
             }
         }
         await Promise.all(promises);
@@ -49,8 +46,9 @@ export class Ladbrokes extends Scraper {
     protected async scrapeMarkets(compId: string, url: string): Promise<Offers> {
         const options: RequestInit = {
             headers: {
-                Origin: "https://www.ladbrokes.com.au",
-                Referer: "https://www.ladbrokes.com.au/"
+                "Content-Type": "application/json",
+                "Origin": "https://www.ladbrokes.com.au",
+                "Referer": "https://www.ladbrokes.com.au/"
             }
         };
         const data = await Scraper.getDataFromUrl(url, options) as MarketsResponse;
@@ -71,7 +69,7 @@ export class Ladbrokes extends Scraper {
     }
 
     protected parseMarketName(name: string): string | false {
-        if (name === "Match Betting") {
+        if (name === "Match Betting" || name === "Head To Head") {
             return "HeadToHead";
         } else if (name.endsWith("Alternate Handicaps")) {
             return "Lines";
@@ -91,8 +89,11 @@ interface MatchesResponse {
             actual_start: string;
             event_type: {
                 name: string;
-            }
-        }
+            };
+            competition: {
+                name: string;
+            };
+        };
     };
 }
 
